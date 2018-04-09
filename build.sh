ec3f66a2225c99a7b4156791d24f03aef7ea5d7c
Eric Sonchaiwanich <eric@galacticfog.com>
2018-04-09 17:08:16 -0400

    Build zip targets

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 echo "Installing npm packages..."
 npm install
 
+rm -rf ./target/linux
+rm -rf ./target/macos
+
+VERSION=`cat package.json | jq -r '.version'`
+
 echo "Building fog for Linux..."
 mkdir -p ./target/linux
 pkg -t node8-linux-x64  -o ./target/linux/fog .
+cd ./target/linux
+zip gestalt-fog-cli-linux-v$VERSION.zip fog
+cd -
 
 echo "Building fog for MacOS..."
 mkdir -p ./target/macos
 pkg -t node8-macos-x64  -o ./target/macos/fog .
+cd ./target/macos
+zip gestalt-fog-cli-macos-v$VERSION.zip fog
+cd -
+
