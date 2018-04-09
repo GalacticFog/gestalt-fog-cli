354f1ddde2bb95d23b9557ea35c4c33b63396098
Eric Sonchaiwanich <eric@galacticfog.com>
2018-04-09 17:12:53 -0400

    move zip location

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 echo "Installing npm packages..."
 npm install
 
-rm -rf ./target/linux
-rm -rf ./target/macos
+rm -rf ./target
 
 VERSION=`cat package.json | jq -r '.version'`
 
@@ -10,13 +9,13 @@ echo "Building fog for Linux..."
 mkdir -p ./target/linux
 pkg -t node8-linux-x64  -o ./target/linux/fog .
 cd ./target/linux
-zip gestalt-fog-cli-linux-v$VERSION.zip fog
+zip ../gestalt-fog-cli-linux-v$VERSION.zip fog
 cd -
 
 echo "Building fog for MacOS..."
 mkdir -p ./target/macos
 pkg -t node8-macos-x64  -o ./target/macos/fog .
 cd ./target/macos
-zip gestalt-fog-cli-macos-v$VERSION.zip fog
+zip ../gestalt-fog-cli-macos-v$VERSION.zip fog
 cd -
 
