c37a2e78074dbfa4a70248b5f3b7dc507e2e24c1
Eric Sonchaiwanich <eric@galacticfog.com>
2018-04-13 15:30:25 -0400

    Make create org scriptable

--- cmds/create_cmds/org.js
+++ cmds/create_cmds/org.js
@@ -6,49 +6,89 @@ const cmd = require('../lib/cmd-base');
 const debug = cmd.debug;
 exports.command = 'org'
 exports.desc = 'Create org'
-exports.builder = {}
+exports.builder = {
+    'org': {
+        alias: 'o',
+        description: 'Parent org fqon'
+    },
+    'name': {
+        alias: 'n',
+        description: 'Org name'
+    },
+    'description': {
+        alias: 'd',
+        description: 'Org description'
+    }
+}
 exports.handler = cmd.handler(async function (argv) {
 
-    const context = await ui.resolveOrg();
+    if (argv.name) {
+        // Command line
 
-    const parent = context.org;
+        // Copy argv properties to workspace spec
+        const orgSpec = {};
+        for (let s of ['name', 'description']) {
+            if (!argv[s]) throw Error(`Missing --${s} property`);
+            orgSpec[s] = argv[s];
+        }
 
-    debug(`parent: ${JSON.stringify(parent, null, 2)}`);
+        // Check if org property is required
+        let parentOrg = argv.org;
+        if (!parentOrg) {
+            const context = gestalt.getContext();
+            if (!context.org || !context.org.fqon) {
+                throw Error(`Missing --org property, not found in current context`);
+            } else {
+                console.log(`Using '${context.org.fqon}' org.`)
+            }
+        }
 
-    const questions = [
-        {
-            message: "Name",
-            type: 'input',
-            name: 'name',
-            validate: inputValidation.resourceName
-        },
-        {
-            message: "Description",
-            type: 'input',
-            name: 'description',
-            validate: inputValidation.resourceDescription
-        },
-        {
-            message: "Proceed?",
-            type: 'confirm',
-            name: 'confirm',
-            default: false
-        },
-    ];
-
-    const answers = await inquirer.prompt(questions);
-
-    debug(`answers: ${answers}`);
-    if (answers.confirm) {
-        const orgSpec = {
-            name: answers.name,
-            description: answers.description
-        };
-
-        const org = await gestalt.createOrg(orgSpec, parent.fqon);
-        debug(`org: ${org}`);
+        // Create Org
+        const org = await gestalt.createOrg(orgSpec, parentOrg);
         console.log(`Org '${org.name}' created.`);
     } else {
-        console.log('Aborted.');
+
+        const context = await ui.resolveOrg();
+
+        const parent = context.org;
+
+        debug(`parent: ${JSON.stringify(parent, null, 2)}`);
+
+        const questions = [
+            {
+                message: "Name",
+                type: 'input',
+                name: 'name',
+                validate: inputValidation.resourceName
+            },
+            {
+                message: "Description",
+                type: 'input',
+                name: 'description',
+                validate: inputValidation.resourceDescription
+            },
+            {
+                message: "Proceed?",
+                type: 'confirm',
+                name: 'confirm',
+                default: false
+            },
+        ];
+
+        const answers = await inquirer.prompt(questions);
+
+        debug(`answers: ${answers}`);
+        if (answers.confirm) {
+            const orgSpec = {
+                name: answers.name,
+                description: answers.description
+            };
+
+            const org = await gestalt.createOrg(orgSpec, parent.fqon);
+            debug(`org: ${org}`);
+            console.log(`Org '${org.name}' created.`);
+        } else {
+            console.log('Aborted.');
+        }
     }
 });
