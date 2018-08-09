8b9b408f36bf4c629898504555a52b38935542a8
John Betancur <johnnyazee@gmail.com>
2018-08-09 17:43:01 -0400

    refactor serverless deploy to work within serverelss yml spec

--- cmds/deploy_cmds/service.js
+++ cmds/deploy_cmds/service.js
@@ -1,6 +1,7 @@
+const chalk = require('chalk');
 const gestalt = require('../lib/gestalt');
 const cmd = require('../lib/cmd-base');
-const { serviceSchema, lambdaSchema, endpointSchema } = require('../lib/schemas');
+const { serviceSchema, lambdaSchema, endpointSchema } = require('../../schemas');
 const { asyncForEach } = require('../lib/helpers');
 
 const command = 'service';
@@ -11,114 +12,109 @@ const builder = {
     description: 'service definition file',
     required: true,
   },
+  filter: {
+    description: 'a',
+  },
 };
 
 const handler = async (argv) => {
-  if (argv.file) {
+  try {
     console.log(`Loading service spec from file ${argv.file}`);
+    const service = serviceSchema.service.cast(cmd.loadYAMLFromFile(argv.file));
+    const { provider, functions } = service;
 
-    try {
-      const service = serviceSchema.cast(cmd.loadYAMLFromFile(argv.file));
-      const { provider, functions } = service;
-      await serviceSchema.validate(service)
-        .catch(e => {
-          throw Error(`Invalid service schema '${e.errors}' in file ${argv.file}`);
-        });
-
-      const context = await cmd.resolveContextPath(provider.context);
-      const resolvedProvider = await cmd.resolveProvider({
-        name: provider.laser,
-        org: context.org.fqon,
-        provider: provider.laser
+    await serviceSchema.service.validate(service)
+      .catch(e => {
+        throw Error(`Invalid service schema '${e.errors}' in file ${argv.file}`);
       });
 
-      const apis = await gestalt.fetchEnvironmentApis(context);
-      const api = apis.find(a => a.name === provider.api);
+    const context = await cmd.resolveContextPath(provider.context);
+    const resolvedProvider = await cmd.resolveProvider({
+      name: provider.laser,
+      org: context.org.fqon,
+      provider: provider.laser,
+    });
 
-      if (!api) {
-        throw Error(`Could not find api with name '${provider.api}'`);
-      }
+    const apis = await gestalt.fetchEnvironmentApis(context);
+    const api = apis.find(a => a.name === provider.api);
 
-      // when using for each need to wrap with async util
-      await asyncForEach(functions, async f => {
-        console.log(`Creating ${f.name} Lambda`);
-
-        const lambda = lambdaSchema.cast({
-          name: f.name,
-          description: f.description,
-          properties: {
-            code_type: 'package',
-            package_url: f['package-url'],
-            handler: f.handler,
-            compressed: f.compressed,
-            headers: f.headers,
-            memory: f.memory,
-            cpus: f.cpus,
-            timeout: f.timeout,
-            public: f.public,
-            runtime: f.runtime,
-            env: f.variables,
-            provider: {
-              id: resolvedProvider.id,
-              locations: [],
-            },
-          },
-        });
-
-        await lambdaSchema.validate(lambda)
-          .catch(e => {
-            throw Error(`Invalid function:lambda schema '${e.errors}'`);
-          });
-
-        const response = await gestalt.createLambda(lambda, context);
-        console.log(`Successfully created ${f.name} Lambda`);
-
-        console.log(`Creating ${f.endpoint.path} Endpoint for ${f.name}`);
-        const endpoint = endpointSchema.cast({
-          name: f.endpoint.path,
-          properties: {
-            implementation_id: response.id,
-            implementation_type: 'lambda',
-            resource: f.endpoint.path,
-            hosts: f.endpoint.hosts || [],
-            methods: f.endpoint.methods || ['GET'],
-            synchronous: f.endpoint.synchronous || true,
-            plugins: {
-              rateLimit: {
-                enabled: f.endpoint['rate-limit'] === -1 ? false : true || false,
-                perMinute: f.endpoint['rate-limit'],
-              },
-              gestaltSecurity: {
-                enabled: f.endpoint.secure || false,
-              },
-            },
-          },
-        });
-
-        await endpointSchema.validate(endpoint)
-          .catch(e => {
-            throw Error(`Invalid function:endpoint schema '${e.errors}'`);
-          });
-
-        await gestalt.createApiEndpoint(endpoint, {
-          org: {
-            fqon: context.org.fqon,
-          },
-          api: {
-            id: api.id,
-          },
-        });
-
-        console.log(`Successfully created ${f.endpoint.path} Endpoint for ${f.name}`);
-      });
-
-      console.log(`Successfully created Service file ${argv.file}`);
-    } catch (e) {
-      console.error(`There was an error creating the Service ${e}`);
+    if (!api) {
+      throw Error(`Could not find api with name '${provider.api}'`);
     }
-  }
 
-  throw Error('A Service Definition file must be provided');
+    await asyncForEach(Object.values(functions), async (func, i) => {
+      const name = Object.keys(functions)[i];
+      const f = serviceSchema.serviceFunction.cast(func);
+      await serviceSchema.serviceFunction.validate(f)
+        .catch(e => {
+          throw Error(`Invalid service: functions schema '${e.errors}' in file ${argv.file}`);
+        });
+
+      console.log(`Creating ${name} Lambda`);
+
+      const lambda = lambdaSchema.cast({
+        name,
+        description: f.description,
+        properties: {
+          code_type: 'package',
+          package_url: f['package-url'],
+          handler: f.handler,
+          compressed: f.compressed,
+          headers: f.headers,
+          memory: f.memory,
+          cpus: f.cpus,
+          timeout: f.timeout,
+          public: f.public,
+          runtime: f.runtime,
+          env: f.variables,
+          provider: {
+            id: resolvedProvider.id,
+            locations: [],
+          },
+        },
+      });
+
+      const response = await gestalt.createLambda(lambda, context);
+      console.log(chalk.green(`Successfully created ${name} Lambda`));
+
+      console.log(`Creating ${f.endpoint.path} Endpoint for ${name}`);
+      const endpoint = endpointSchema.cast({
+        name: f.endpoint.path,
+        properties: {
+          implementation_id: response.id,
+          implementation_type: 'lambda',
+          resource: f.endpoint.path,
+          hosts: f.endpoint.hosts || [],
+          methods: f.endpoint.methods || ['GET'],
+          synchronous: f.endpoint.synchronous || true,
+          plugins: {
+            rateLimit: {
+              enabled: f.endpoint['rate-limit'] === -1 ? false : true || false,
+              perMinute: f.endpoint['rate-limit'],
+            },
+            gestaltSecurity: {
+              enabled: f.endpoint.secure || false,
+            },
+          },
+        },
+      });
+
+      await gestalt.createApiEndpoint(endpoint, {
+        org: {
+          fqon: context.org.fqon,
+        },
+        api: {
+          id: api.id,
+        },
+      });
+
+      console.log(chalk.green(`Successfully created ${f.endpoint.path} Endpoint for ${name}`));
+    });
+
+    console.log(chalk.green(`Successfully created Service file ${argv.file}`));
+  } catch (e) {
+    console.error(chalk.red(`There was an error creating the Service ${e}`));
+  }
 };
 
 module.exports = {
