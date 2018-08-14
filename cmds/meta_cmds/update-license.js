756f22449f018f45e73c575111f686dab582a7a0
Eric Sonchaiwanich <eric@galacticfog.com>
2018-08-13 23:36:06 -0400

    Implemented 'fog meta update-license' and improved 'fog meta POST'

---
+++ cmds/meta_cmds/update-license.js
@@ -0,0 +1,36 @@
+const gestalt = require('../lib/gestalt')
+const cmd = require('../lib/cmd-base');
+const debug = cmd.debug;
+const util = require('../lib/util');
+
+
+exports.command = 'update-license'
+exports.desc = 'Update Gestalt license'
+exports.builder = {
+    file: {
+        alias: 'f',
+        description: 'resource definition file',
+    }
+}
+
+exports.handler = cmd.handler(async function (argv) {
+    if (!argv.file) {
+        throw Error('missing --file parameter');
+    }
+
+    console.log('Loading license from file...');
+    // const data = util.readFileAsText(argv.file);
+
+    // const spec = {
+    //     "name": "Default-License-1",
+    //     "description": "Default GF license",
+    //     "properties": {
+    //         "data": data
+    //     }
+    // }
+
+    const spec = util.loadObjectFromFile(argv.file);
+
+    const res = await gestalt.metaPost('/root/licenses', spec);
+    console.log("Gestalt Platform license udpated.")
+});
