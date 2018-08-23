7e13690d4d829e858362a1ec1b2828ca293b0868
Eric Sonchaiwanich <eric@galacticfog.com>
2018-08-23 16:24:38 -0400

    Add alpine build target

--- build.sh
+++ build.sh
@@ -5,17 +5,13 @@ rm -rf ./target
 
 VERSION=`cat package.json | jq -r '.version'`
 
-echo "Building fog for Linux..."
-mkdir -p ./target/linux
-pkg -t node8-linux-x64  -o ./target/linux/fog .
-cd ./target/linux
-zip ../gestalt-fog-cli-linux-$VERSION.zip fog
-cd -
+os_list="linux alpine macos"
 
-echo "Building fog for MacOS..."
-mkdir -p ./target/macos
-pkg -t node8-macos-x64  -o ./target/macos/fog .
-cd ./target/macos
-zip ../gestalt-fog-cli-macos-$VERSION.zip fog
-cd -
-
+for os in ${os_list}; do
+    echo "Building fog for target $os..."
+    mkdir -p ./target/$os
+    pkg -t node8-${os}-x64  -o ./target/${os}/fog .
+    cd ./target/${os}
+    zip ../gestalt-fog-cli-${os}-$VERSION.zip fog
+    cd -
+done
