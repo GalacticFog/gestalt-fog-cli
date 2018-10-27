41bd9ad8d80ed2d7ee98cc9e3aaa52c6e0feacb3
Eric Sonchaiwanich <eric@galacticfog.com>
2018-10-26 21:38:55 -0400

    Improve scale command

--- cmds/scale_cmds/scale_container.js
+++ cmds/scale_cmds/scale_container.js
@@ -1,117 +1,100 @@
 const gestalt = require('../lib/gestalt')
 const ui = require('../lib/gestalt-ui')
-const selectResource = require('../lib/selectResourceUI');
 const inquirer = require('inquirer');
 const cmd = require('../lib/cmd-base');
-exports.command = 'container'
+
+exports.command = 'container [container_name] [num_instances]'
 exports.desc = 'Scale container'
 exports.builder = {}
 exports.handler = cmd.handler(async function (argv) {
 
+    // fog migrate container nginx1 /root/default-ecs
+
     // Main
-    if (argv.fqon || argv.id || argv.instances) {
+    if (argv.container_name || argv.num_instances) {
         // Command mode
 
-        if (!argv.fqon) throw Error("missing argv.fqon");
-        if (!argv.id) throw Error("missing argv.id");
-        if (!argv.instances) throw Error("missing argv.instances");
+        if (!argv.container_name) throw Error('missing container_name')
+        if (!argv.num_instances) throw Error('missing num_instances')
 
-        const num_instances = argv.instances;
+        const context = await cmd.resolveEnvironment(argv, gestalt.getContext());
 
-        if (!validate(num_instances)) {
-            return;
-        }
+        validate(argv.num_instances);
 
-        const container = await gestalt.fetchContainer({ fqon: argv.fqon, id: argv.id });
-        container.properties.num_instances = num_instances;
-        await gestalt.updateContainer(container);
-        console.log('done.');
+        const container = await gestalt.fetchContainer({ name: argv.container_name }, context);
 
+        container.properties.num_instances = argv.num_instances;
+        const response = await gestalt.updateContainer(container);
+        console.log(`Container '${container.name}' scale to ${argv.num_instances} initiated.`);
     } else {
 
+        // Interactive mode
+
         const context = await ui.resolveEnvironment();
-
-        const container = await selectContainer(context);
-        const num_instances = await getUserInput();
-        // Validate input
-        if (!validate(num_instances)) {
-            return;
-        }
-
-        // Confirmation step
-        if (await confirmIfNeeded(num_instances)) {
-            // Scale up
-            container.properties.num_instances = num_instances;
-            const c = await gestalt.updateContainer(container, context);
-            console.log("Done.");
-            console.log();
-            console.log(`The following command may be run to scale the container directly:`);
-            console.log();
-            console.log(`    ${argv['$0']} ${argv._[0]} ${argv._[1]} --fqon ${container.org.properties.fqon} --id ${container.id} --instances ${num_instances}`);
-            console.log();
-        } else {
-            console.log("Aborted");
-        }
-    }
-
-    function validate(num_instances) {
-        if (!num_instances) {
-            console.log(`Invalid input: num_instances is '${num_instances}', aborting.`);
-            return false;
-        }
-        if (!Number.isInteger(Number(num_instances))) {
-            console.log(`Invalid input: '${num_instances}' is not an integer, aborting.`);
-            return false;
-        }
-        if (num_instances < 0) {
-            console.log(`Invalid input: num_instances is '${num_instances}', aborting.`);
-            return false;
-        }
-        return true;
-    }
-
-    async function selectContainer(context) {
-
-        const res = await gestalt.fetchContainers(context);
+        const containers = await gestalt.fetchContainers(context);
 
         let options = {
             mode: 'autocomplete',
             message: "Select Container(s)",
             fields: ['name', 'properties.status', 'properties.image', 'properties.num_instances', 'owner.name', 'properties.provider.name'],
-            sortBy: 'name',
-            resources: res
+            sortBy: 'name'
         }
+    
+        const container = await ui.selectContainer(options, containers);
+        const num_instances = await getUserInput();
+        // Validate input
+        validate(num_instances);
 
-        return selectResource.run(options);
-    }
-
-    function getUserInput() {
-        const questions = [
-            {
-                message: "Number of instances",
-                type: 'input',
-                name: 'num_instances',
-            },
-        ];
-
-        return inquirer.prompt(questions).then(answers => answers.num_instances);
-    }
-
-    function confirmIfNeeded(num_instances) {
-        if (num_instances > 10) {
-            const questions = [
-                {
-                    message: `Scaling to ${num_instances}, are you sure?`,
-                    type: 'confirm',
-                    name: 'confirm',
-                    default: false
-                },
-            ];
-
-            return inquirer.prompt(questions).then(answers => answers.confirm);
+        // Confirmation step
+        if (await confirmIfNeeded(num_instances)) {
+            // Scale up
+            container.properties.num_instances = num_instances;
+            const response = await gestalt.updateContainer(container, context);
+            console.log(`Container '${container.name}' scale to ${num_instances} initiated.`);
         } else {
-            // No need for confirmation
-            return new Promise(resolve => resolve(true));
+            console.log("Aborted");
         }
     }
 });
+
+function validate(num_instances) {
+    if (!num_instances) {
+        throw Error(`Invalid input: num_instances is '${num_instances}', aborting.`);
+    }
+    if (!Number.isInteger(Number(num_instances))) {
+        throw Error(`Invalid input: '${num_instances}' is not an integer, aborting.`);
+    }
+    if (num_instances < 0) {
+        throw Error(`Invalid input: num_instances is '${num_instances}', aborting.`);
+    }
+}
+
+function getUserInput() {
+    const questions = [
+        {
+            message: "Number of instances",
+            type: 'input',
+            name: 'num_instances',
+        },
+    ];
+
+    return inquirer.prompt(questions).then(answers => answers.num_instances);
+}
+
+function confirmIfNeeded(num_instances) {
+    if (num_instances > 10) {
+        const questions = [
+            {
+                message: `Scaling to ${num_instances}, are you sure?`,
+                type: 'confirm',
+                name: 'confirm',
+                default: false
+            },
+        ];
+
+        return inquirer.prompt(questions).then(answers => answers.confirm);
+    } else {
+        // No need for confirmation
+        return new Promise(resolve => resolve(true));
+    }
+}
