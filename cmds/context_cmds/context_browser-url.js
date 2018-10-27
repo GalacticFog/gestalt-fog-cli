aa6aa42fb56c7a5354caf9b4f1eb01704e0e1b8b
Eric Sonchaiwanich <eric@galacticfog.com>
2018-10-26 21:21:57 -0400

    Improve browser-url

--- cmds/context_cmds/context_browser-url.js
+++ cmds/context_cmds/context_browser-url.js
@@ -1,11 +1,13 @@
 'use strict';
 const cmd = require('../lib/cmd-base');
 const gestaltContext = require('../lib/gestalt-context');
-exports.command = 'get-browser-url'
+const chalk = require('../lib/chalk');
+exports.command = 'get-browser-url [path]'
 exports.desc = 'get-browser-url'
 exports.builder = {}
 exports.handler = cmd.handler(async function (argv) {
-    const url = gestaltContext.getBrowserUrl();
-    console.log(url);
+    const context = argv.path? await cmd.resolveContextPath(argv.path) : gestaltContext.getContext();
+    const url = gestaltContext.getBrowserUrl(context);
+    console.log(chalk.bold.blue(url));
 });
 
