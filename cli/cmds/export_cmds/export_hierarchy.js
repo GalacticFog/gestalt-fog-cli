52367dee1a45d2519051c32a91e2b81c79ac1ff0
Eric Sonchaiwanich <eric@galacticfog.com>
2019-04-04 19:47:17 +0000

    Skip interactivity and allow specifying resource types

--- cli/cmds/export_cmds/export_hierarchy.js
+++ cli/cmds/export_cmds/export_hierarchy.js
@@ -1,6 +1,7 @@
 const { gestalt, gestaltSession } = require('gestalt-fog-sdk')
 const ui = require('../lib/gestalt-ui')
 const cmd = require('../lib/cmd-base');
+const { debug } = require('../lib/debug');
 const { doExportHierarchy, getDefaultExportDirectory } = require('./exportHelper');
 const fs = require('fs');
 
@@ -21,6 +22,18 @@ exports.builder = {
     raw: {
         type: 'boolean',
         description: 'Output raw resources'
+    },
+    interactive: {
+        type: 'boolean',
+        default: true,
+        description: 'Interactive'
+    },
+    'resource-types': {
+        type: 'string',
+        description: 'Resource types to export'
+    },
+    all: {
+
     }
 }
 
@@ -29,14 +42,14 @@ exports.handler = cmd.handler(async function (argv) {
 
     const basePath = argv.directory || getDefaultExportDirectory(argv.raw);
 
-    if (await checkIfDirectoryExists(basePath)) {
+    const resourceTypes = await getResourceTypes(argv);
 
-        const resourceTypes = await getResourceTypes(argv);
+    if (!argv.interactive || await checkIfDirectoryExists(basePath)) {
 
         if (!argv.output) {
             if (argv.raw) argv.output = 'json';
         }
-    
+
         await doExportHierarchy(context, resourceTypes, basePath, argv.output, argv.raw);
     } else {
         console.log('Aborted.');
@@ -44,6 +57,12 @@ exports.handler = cmd.handler(async function (argv) {
 });
 
 async function checkIfDirectoryExists(basePath) {
+
+    const config = gestaltSession.getGlobalConfig();
+    if (config['interactive'] == 'false') {
+        return true;
+    }
+
     if (fs.existsSync(basePath)) {
         return ui.promptToContinue(`Directory '${basePath}' exists, files may be overwritten. Proceed to export?`, false);
     }
@@ -55,10 +74,31 @@ async function checkIfDirectoryExists(basePath) {
 async function getResourceTypes(argv) {
     const types = gestalt.getEnvironmentResourceTypes();
     types.push('entitlements');
-    if (!argv.all) {
+
+    if (argv.all && argv['resource-types']) {
+        throw Error(`Can only specify one of --all, --resource-types`);
+    }
+
+    if (argv.all) {
+        return types;
+    }
+
+    if (argv['resource-types']) {
+        const specifiedTypes = argv['resource-types'].split(',');
+        for (const t of specifiedTypes) {
+            if (!types.includes(t)) {
+                throw Error(`Specified type is not valid: ${t}`);
+            }
+        }
+        return specifiedTypes;
+    }
+
+    // otherwise, prompt
+    if (argv.interactive) {
         return await ui.selectOptions('Resources to export', types);
     }
-    return types;
+
+    throw Error(`Must specify --resource-types since --interactive is 'false'`);
 }
 
 async function resolveContext(argv) {
